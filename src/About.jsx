76c1439c5e4bf200de2a9b0d7179f5c04ddf1caa
Arko <arkoroyb@gmail.com>
2024-08-17 14:34:38 +0600

    modify

--- src/About.jsx
+++ src/About.jsx
@@ -5,18 +5,32 @@ import { useNavigate } from "react-router-dom";
 const AboutPage = () => {
   const navigate = useNavigate();
 
+  const handleView = () => {
+    const eventKey = "unique_button_click";
+
+    if (!localStorage.getItem(eventKey)) {
+      localStorage.setItem(eventKey, "true");
+
+      ReactGA.event({
+        category: "User",
+        action: "Clicked My Button",
+        label: "Button Click",
+      });
+
+      navigate("/blog");
+    }
+  };
+
   useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+    const pageViewKey = `page_view_${window.location.pathname}`;
+
+    if (!localStorage.getItem(pageViewKey)) {
+      localStorage.setItem(pageViewKey, "true");
+
+      ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+    }
   }, []);
 
-  const handleView = () => {
-    ReactGA.event({
-      category: "User",
-      action: "Clicked My Button",
-      label: "Button Click",
-    });
-    navigate("/blog");
-  };
   return (
     <div>
       this is about page
