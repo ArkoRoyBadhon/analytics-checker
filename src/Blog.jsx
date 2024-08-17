efd2376bb71596c95d0815211cbd8c952de542b4
Arko <arkoroyb@gmail.com>
2024-08-17 14:38:26 +0600

    ss

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
+
 
 const Blog = () => {
+
+    useEffect(() => {
+        const pageViewKey = `page_view_${window.location.pathname}`;
+    
+        if (!localStorage.getItem(pageViewKey)) {
+          localStorage.setItem(pageViewKey, "true");
+    
+          ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+        }
+      }, []);
     return (
         <div>
             This is blog page
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 
 const HomePage = () => {
+    useEffect(() => {
+        const pageViewKey = `page_view_${window.location.pathname}`;
+    
+        if (!localStorage.getItem(pageViewKey)) {
+          localStorage.setItem(pageViewKey, "true");
+    
+          ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+        }
+      }, []);
     return (
         <div>
             This is home page
